420316a053949be40077553c27a1fc14605ff010
suharyadi <suharyadi21@outlook.com>
2024-06-29 10:35:29 +0700

    - update browser remote

--- service_one/service_one.js
+++ service_one/service_one.js
@@ -17,7 +17,7 @@ async function initializeClientOne(authen = false) {
         }),
         webVersionCache: {
             type: 'remote',
-            remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html',
+            remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2413.51-beta.html',
         }
     });
     
