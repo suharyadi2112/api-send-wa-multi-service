fd64b3ca952b8221d5c3cad609ccdae8346c9129
suharyadi <suharyadi21@outlook.com>
2024-06-30 20:53:14 +0700

    - add service backup

--- service_two/index.js
+++ service_two/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const {startAuthenticationtwo, getClientInfotwo, sendWaServicetwo, getReconnecting} = require('./service_two');
+const {sendWaServiceBackup} = require('../service_backup');
 const logger = require('../config/logger');//log
 const { authenticateToken } = require('../config/jwtoken');
 const { limiter } = require('../config/ratelimiter');
@@ -12,8 +13,12 @@ app.use(bodyParser.json());
 app.get('/two/status-service',authenticateToken, limiter, getClientInfotwo);
 // Endpoint untuk menghasilkan QR code
 app.get('/two/generate-qr-service',authenticateToken, limiter, startAuthenticationtwo);
+
+
 // Endpoint untuk kirim pesan pada service two
 app.post('/two/send-service',authenticateToken, limiter, sendWaServicetwo)
+// app.post('/two/send-service', limiter, sendWaServiceBackup)
+
 //reconnecting wa
 app.get('/two/recon-service',authenticateToken, limiter, getReconnecting)
 
